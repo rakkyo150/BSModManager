36702a7e15ded512c83ed81d40d21a299687b6a8
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2022-06-02 19:16:31 +0900

    ファイルハッシュを使って、メタデータがミスっててもInstalledバージョンを適切な状態に保てるようにした

--- BSModManager/Models/ModDisposer.cs
+++ BSModManager/Models/ModDisposer.cs
@@ -1,7 +1,9 @@
 ﻿using BSModManager.Static;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 
 namespace BSModManager.Models
 {
@@ -12,26 +14,38 @@ namespace BSModManager.Models
             if (Directory.GetFiles(sourceDirFullPath).Length == 0) return;
 
             // https://github.com/denpadokei/LocalModAssistant/blob/b0c119f7e32a35cd15ca2010f9dc50b8267183fe/LocalModAssistant/Models/MainViewDomain.cs
-            foreach (var dllFileFullPath in Directory.EnumerateFiles(sourceDirFullPath, "*.dll", SearchOption.TopDirectoryOnly))
+            DisposeDllFile(sourceDirFullPath, destDirFullPath);
+            DisposeZipFile(sourceDirFullPath, destDirFullPath);
+        }
+
+        public void MoveFolder(string sourceDirFullPath, string destDirFullPath)
+        {
+            DirectoryInfo sourceDir = new DirectoryInfo(sourceDirFullPath);
+            
+            DirectoryInfo[] dirs = sourceDir.GetDirectories();
+
+            if (dirs.Length == 0) return;
+
+            foreach(var dir in dirs)
             {
-                if (!Directory.Exists(Path.Combine(destDirFullPath, "Plugins")))
-                {
-                    Directory.CreateDirectory(Path.Combine(destDirFullPath, "Plugins"));
-                }
                 try
                 {
-                    var installPath = Path.Combine(destDirFullPath, "Plugins", Path.GetFileName(dllFileFullPath));
-                    if (File.Exists(installPath))
-                    {
-                        File.Delete(installPath);
-                    }
-                    File.Move(dllFileFullPath, installPath);
+                    Folder.Instance.Copy(dir.FullName, Path.Combine(destDirFullPath,dir.Name), true);
+                    Directory.Delete(dir.FullName, true);
                 }
-                catch (Exception ex)
+                catch(Exception ex)
                 {
-                    Logger.Instance.Error($"{ex.Message}\nダウンロードしたModを正常に移動できませんでした");
+                    Logger.Instance.Info($"{ex.Message}\nダウンロードしたディレクトリの配置に失敗しました");
                 }
             }
+        }
+
+        private static void DisposeZipFile(string sourceDirFullPath, string destDirFullPath)
+        {
+            IEnumerable<string> zipFilesPath = Directory.EnumerateFiles(sourceDirFullPath, "*.zip", SearchOption.TopDirectoryOnly);
+
+            if (zipFilesPath.Count() == 0) return;
+            
             foreach (var zipFileName in Directory.EnumerateFiles(sourceDirFullPath, "*.zip", SearchOption.TopDirectoryOnly))
             {
                 try
@@ -54,6 +68,34 @@ namespace BSModManager.Models
                 catch (Exception ex)
                 {
                     Logger.Instance.Error($"{ex.Message}\nダウンロードしたModの解凍を正常に行えませんでした");
+                }
+            }
+        }
+
+        private static void DisposeDllFile(string sourceDirFullPath, string destDirFullPath)
+        {
+            IEnumerable<string> dllFilesPath = Directory.EnumerateFiles(sourceDirFullPath, "*.dll", SearchOption.TopDirectoryOnly);
+
+            if (dllFilesPath.Count() == 0) return;
+
+            foreach (var dllFileFullPath in dllFilesPath)
+            {
+                if (!Directory.Exists(Path.Combine(destDirFullPath, "Plugins")))
+                {
+                    Directory.CreateDirectory(Path.Combine(destDirFullPath, "Plugins"));
+                }
+                try
+                {
+                    var installPath = Path.Combine(destDirFullPath, "Plugins", Path.GetFileName(dllFileFullPath));
+                    if (File.Exists(installPath))
+                    {
+                        File.Delete(installPath);
+                    }
+                    File.Move(dllFileFullPath, installPath);
+                }
+                catch (Exception ex)
+                {
+                    Logger.Instance.Error($"{ex.Message}\nダウンロードしたModを正常に移動できませんでした");
                 }
             }
         }
---
+++ BSModManager/Static/FileHashProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSModManager.Static
+{
+    internal static class FileHashProvider
+    {
+        static readonly HashAlgorithm hashProvider = new SHA256CryptoServiceProvider();
+
+        // https://mseeeen.msen.jp/compute-hash-string-of-files/
+        /// <summary>
+        /// Returns the hash string for the file.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        internal static string ComputeFileHash(string filePath)
+        {
+            using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                var bs = hashProvider.ComputeHash(fs);
+                return BitConverter.ToString(bs).ToLower().Replace("-", "");
+            }
+        }
+    }
+}
